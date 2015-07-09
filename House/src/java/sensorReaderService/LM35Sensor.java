70f3fe68bbce3ee122dea4f9e8c9fe37b3dced0b
diego-dell <diego-dell@Inspirion>
2015-07-09 09:54:07 -0300

    Adicionado Leitura de Sensor

---
+++ House/src/java/sensorReaderService/LM35Sensor.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sensorReaderService;
+
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author diego-dell
+ */
+public class LM35Sensor {
+
+    SerialPort serialPort;
+    /**
+     * The port we're normally going to use.
+     */
+    private static final String PORT_NAMES[] = {
+        //"/dev/tty.usbserial-A9007UX1", // Mac OS X
+        "/dev/ttyUSB0" // Linux
+    //"COM3", // Windows
+    };
+    /**
+     * A BufferedReader which will be fed by a InputStreamReader converting the
+     * bytes into characters making the displayed results codepage independent
+     */
+    private BufferedReader input;
+    /**
+     * The output stream to the port
+     */
+    private OutputStream output;
+    /**
+     * Milliseconds to block while waiting for port open
+     */
+    private static final int TIME_OUT = 2000;
+    /**
+     * Default bits per second for COM port.
+     */
+    private static final int DATA_RATE = 9600;
+
+    public void initialize() {
+        CommPortIdentifier portId = null;
+        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+
+        //First, Find an instance of serial port as set in PORT_NAMES.
+        while (portEnum.hasMoreElements()) {
+            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+            for (String portName : PORT_NAMES) {
+                if (currPortId.getName().equals(portName)) {
+                    portId = currPortId;
+                    break;
+                }
+            }
+        }
+        if (portId == null) {
+            System.out.println("Could not find COM port.");
+            return;
+        }
+
+        try {
+            // open serial port, and use class name for the appName.
+            serialPort = (SerialPort) portId.open(this.getClass().getName(),
+                    TIME_OUT);
+
+            // set port parameters
+            serialPort.setSerialPortParams(DATA_RATE,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+
+            // open the streams
+            input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+            output = serialPort.getOutputStream();
+
+            // add event listeners
+            //serialPort.addEventListener(this);
+            serialPort.notifyOnDataAvailable(true);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
+    }
+
+    /**
+     * This should be called when you stop using the port. This will prevent
+     * port locking on platforms like Linux.
+     */
+    public synchronized void close() {
+        if (serialPort != null) {
+            serialPort.removeEventListener();
+            serialPort.close();
+        }
+    }
+
+    /**
+     * Handle an event on the serial port. Read the data and print it.
+     *
+     * @return
+     */
+    public synchronized String getValue() {
+        try {
+            return input.readLine();
+            // Ignore all the other eventTypes, but you should consider the other ones.
+        } catch (IOException ex) {
+            Logger.getLogger(LM35Sensor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LM35Sensor servico = new LM35Sensor();
+        servico.initialize();
+
+        Thread t = new Thread() {
+
+            @Override
+            public void run() {
+                //the following line will keep this app alive for 1000 seconds,
+                //waiting for events to occur and responding to them (printing incoming messages to console).
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                        System.out.println(servico.getValue());
+
+                    } catch (InterruptedException ie) {
+                    }
+                }
+            }
+        };
+        t.start();
+        System.out.println("Started");
+    }
+
+}
